0b18a84ecdaaa813ff1b940960a22033e8ff5c5d
Zoey0314y <70824527+Zoey0314y@users.noreply.github.com>
2021-01-17 21:26:05 -0600

    Add files via upload

---
+++ screens/BookTransactionScreen.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, TextInput, Image, StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class BookTransactionScreen extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        hasCameraPermissions: null,
+        scanned: false,
+        scannedStoryTitle: '',
+        scannedAuthor:'',
+        buttonState: 'normal'
+      }
+    }
+
+    getCameraPermissions = async (id) =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA);
+      
+      this.setState({
+        /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+        hasCameraPermissions: status === "granted",
+        buttonState: id,
+        scanned: false
+      });
+    }
+
+    handleBarCodeScanned = async({type, data})=>{
+      const {buttonState} = this.state
+
+      if(buttonState==="BookId"){
+        this.setState({
+          scanned: true,
+          scannedStoryTitle: data,
+          buttonState: 'normal'
+        });
+      }
+      else if(buttonState==="StudentId"){
+        this.setState({
+          scanned: true,
+          scannedAuthor: data,
+          buttonState: 'normal'
+        });
+      }
+      
+    }
+
+    render() {
+      const hasCameraPermissions = this.state.hasCameraPermissions;
+      const scanned = this.state.scanned;
+      const buttonState = this.state.buttonState;
+
+      if (buttonState !== "normal" && hasCameraPermissions){
+        return(
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        );
+      }
+
+      else if (buttonState === "normal"){
+        return(
+          <View style={styles.container}>
+            <View>
+            <Header
+          backgroundColor='pink'
+          centerComponent={{
+            text: 'Story Hub',
+            style: { color: '#fff', fontSize: 20 },
+          }}/>
+              <Text style={{textAlign: 'center', fontSize: 30}}>WILY</Text>
+            </View>
+            <View style={styles.inputView}>
+            <TextInput
+              style={styles.inputBox}
+              placeholder="Story Title"
+              value={this.state.scannedBookId}/>
+            <TouchableOpacity 
+              style={styles.scanButton}
+              onPress={()=>{
+                this.getCameraPermissions("StoryTitle")
+              }}>
+              <Text style={styles.buttonText}>Scan</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={styles.inputView}>
+            <TextInput 
+              style={styles.inputBox}
+              placeholder="Author"
+              value={this.state.scannedStudentId}/>
+            <TouchableOpacity 
+              style={styles.scanButton}
+              onPress={()=>{
+                this.getCameraPermissions("Author")
+              }}>
+              <Text style={styles.buttonText}>Scan</Text>
+            </TouchableOpacity>
+            </View>
+          </View>
+        );
+      }
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    displayText:{
+      fontSize: 15,
+      textDecorationLine: 'underline'
+    },
+    scanButton:{
+      backgroundColor: '#2196F3',
+      padding: 10,
+      margin: 10
+    },
+    buttonText:{
+      fontSize: 15,
+      textAlign: 'center',
+      marginTop: 10
+    },
+    inputView:{
+      flexDirection: 'row',
+      margin: 20
+    },
+    inputBox:{
+      width: 200,
+      height: 40,
+      borderWidth: 1.5,
+      borderRightWidth: 0,
+      fontSize: 20
+    },
+    scanButton:{
+      backgroundColor: '#66BB6A',
+      width: 50,
+      borderWidth: 1.5,
+      borderLeftWidth: 0
+    }
+  });
